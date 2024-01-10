b586c0b962ef9b07bbeeb35cb73ce8d8bd9d653d
Kevin <kevin.suarez.melendez@gmail.com>
2024-01-10 07:35:34 -0500

    Add QR Code

--- src/app/scan/page.tsx
+++ src/app/scan/page.tsx
@@ -1,19 +1,19 @@
 import ToastHandler from "@/components/organism/ToastHandler";
 import WifiDetails from "@/components/organism/WifiDetails";
+import Image from "next/image";
 import Link from "next/link";
 
-export default function Home() {
+export default function Scan() {
   return (
     <main className="absolute top-0 z-[-2] h-screen w-screen bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]">
-      <div className="absolute top-0 right-0 p-4">
-        <Link href="/scan" className="text-gray-600 underline">
-          Scan
-        </Link>
-      </div>
       <div className="text-white flex min-h-screen flex-col items-center justify-center p-16">
-        <WifiDetails />
+        <Image
+          src="/assets/qr-code.png"
+          alt="QR Code"
+          width="360"
+          height="360"
+        />
       </div>
-      <ToastHandler />
     </main>
   );
 }
